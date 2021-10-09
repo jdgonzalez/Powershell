b737b27be7e353b3d703e12277e7ff480e69d0d8
J.D. Gonzalez <jdgonzalez@jdandrachel.com>
2021-10-08 20:06:29 -0500

    initial check in

---
+++ get-sessionapistatus.ps1
@@ -0,0 +1,11 @@
+
+
+for ($i=0; $i -lt 2000; $i++)
+{
+    $status = Invoke-WebRequest -Uri "https://sessionapi.paymetric.com/ping"
+    if ($status.statusCode -ne "200")
+    {
+        Write-Host "Error"
+        break
+    }
+}
