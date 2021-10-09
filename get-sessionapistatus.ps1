e0dacef310d0f2af77bfc41999e641529d4989db
J.D. Gonzalez <jdgonzalez@jdandrachel.com>
2021-10-08 23:14:29 -0500

    removing spaces

--- get-sessionapistatus.ps1
+++ get-sessionapistatus.ps1
@@ -1,8 +1,6 @@
-
-
 for ($i=0; $i -lt 2000; $i++)
 {
-    $status = Invoke-WebRequest -Uri "https://sessionapi.paymetric.com/ping"
+    $status = Invoke-WebRequest -Uri "https://sessionapi.paymetric.com/ping" -InformationAction SilentlyContinue
     if ($status.statusCode -ne "200")
     {
         Write-Host "Error"
